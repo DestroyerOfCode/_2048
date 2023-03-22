783b246bc5d14b560144d3428aeb9c61c826582f
DestroyerOfCode <marius.babkovic9@gmail.com>
2023-03-22 07:58:23 +0100

    made gameboard an instance variable in GameboardViewModel

--- src/main/kotlin/_2048/Main.kt
+++ src/main/kotlin/_2048/Main.kt
@@ -4,6 +4,7 @@ import _2048.gameboard.GameBoard
 import _2048.gameboard.GameBoardService
 import _2048.gameboard.GameBoardServiceImpl
 import _2048.ui.component.GameBoardViewModel
+import _2048.ui.view.GameBoardView
 import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.ColorPainter
@@ -16,11 +17,13 @@ fun main() = singleWindowApplication(title = "2048", icon = ColorPainter(Color.C
 
     val gameBoard = GameBoard()
     val gameBoardService: GameBoardService = GameBoardServiceImpl(gameBoard)
+    val gameBoardViewModel = GameBoardViewModel(gameBoard)
+    val gameBoardView = GameBoardView(gameBoardViewModel)
+
     CoroutineScope(Dispatchers.Default).launch {
         gameBoardService.playGame()
     }
 
-    val gameBoardView = GameBoardViewModel(gameBoard)
     MaterialTheme {
         gameBoardView.playingBoard()
     }
--- src/main/kotlin/_2048/ui/component/GameBoardViewModel.kt
+++ src/main/kotlin/_2048/ui/component/GameBoardViewModel.kt
@@ -4,7 +4,7 @@ import _2048.gameboard.GameBoard
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 
-class GameBoardViewModel(gameBoard: GameBoard) {
+class GameBoardViewModel(var gameBoard: GameBoard) {
 
     var gameBoardBox: MutableState<Array<IntArray>> = mutableStateOf(gameBoard.playingArea)
         private set
